d439cd952b2d4eaf3b4e056dc01a6b4e033004c3
BeccaSR <144284901+BeccaSR@users.noreply.github.com>
2024-02-14 20:20:44 +0000

    Add files via upload

---
+++ Day 17.py
@@ -0,0 +1,9 @@
+# Day 17 Challenge:
+# Create a program that capitalizes the first letter of each word in a sentence.
+
+# initialise user_sentence function with user input of a sentence
+# use .title() to capitalise each word of the sentence
+user_sentence = input("Enter your sentence:").title()
+
+# print capitalised version of the users sentence
+print(user_sentence)
